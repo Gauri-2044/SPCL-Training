332ef8c8fcac635a63b44e6f90dd7dd2bc95ce10
Gauri-2044 <gauribhasme43@gmail.com>
2026-02-17 05:13:34 -0800

    Portfolio Initialized

---
+++ Week01-Portfolio/js/data.js
@@ -0,0 +1,82 @@
+const portfolioData = {
+
+    // ---------------hero section--------------------
+  hero: {
+    name: "",
+    role: "",
+    summary: "",
+    ctaPrimary: "",
+    ctaSecondary: ""
+  },
+
+  // ---------------about section--------------------
+  about: {
+    intro: "",
+    strengths: [
+      
+    ],
+    goals: ""
+  },
+
+  // ---------------skills section--------------------
+  skills: [
+    {
+      category: "",
+      items: []
+    },
+    {
+      category: "",
+      items: []
+    },
+  ],
+
+  // ---------------projects section--------------------
+  projects: [
+    {
+      title: "",
+      problem: "",
+      approach: "",
+      tech: [],
+      result: "",
+      reflection: "",
+      github: "#",
+      demo: "#"
+    }
+  ],
+
+  // ---------------experience section--------------------
+  experience: [
+    {
+      role: "",
+      organization: "",
+      responsibilities: [
+        
+      ],
+      skillsUsed: [],
+      outcome: ""
+    }
+  ],
+
+  // ---------------education section--------------------
+  education: [
+    {
+      degree: "",
+      institution: "",
+      highlights: []
+    }
+  ],
+
+  // ---------------certifications section--------------------
+
+  certifications: [
+  ],
+
+  // ---------------contact section--------------------
+
+  contact: {
+    email: "",
+    linkedin: "#",
+    github: "#"
+  }
+
+};
